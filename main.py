a968e17b4834b15e55a48910820eaeec5262d9cd
Orangeliquid <hardym2020111@gmail.com>
2024-02-16 03:18:25 -0500

    Remove FAQs from header/footer.html, add many styles to styles.css, create cpg.html, add endpoint cpg.html and logic to analyse uploaded image in main.py, adjust logic in color_palette_generator.py to display all found colors instead of filtered similars

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
+from color_palette_generator import get_most_common_colors
+import os
+import shutil
+import atexit
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -15,7 +19,39 @@ def home():
 def about():
     return render_template("about.html")
 
-# Functionality
+
+@app.route("/cpg", methods=['GET', 'POST'])
+def cpg():
+    most_common_colors = None
+
+    if request.method == 'POST':
+        if 'imageUpload' in request.files:
+            image_file = request.files['imageUpload']
+            if image_file.filename != '':
+                try:
+                    # Create a temporary folder
+                    temp_folder = 'static/temp_folder'
+                    os.makedirs(temp_folder, exist_ok=True)
+
+                    # Check if the file is allowed
+                    allowed_extensions = {'png', 'jpg', 'jpeg'}
+                    if ('.' in image_file.filename and image_file.filename.rsplit('.', 1)[1].lower()
+                            in allowed_extensions):
+                        # Save the uploaded file to the temporary folder
+                        image_path = os.path.join(temp_folder, 'temp_image.jpg')
+                        image_file.save(image_path)
+
+                        # Get the number of colors from the form
+                        num_colors = int(request.form.get('numColors', 5))
+
+                        # Generate color palette
+                        most_common_colors = get_most_common_colors(image_path, num_colors=num_colors)
+
+                finally:
+                    # Remove the temporary folder and its contents
+                    shutil.rmtree(temp_folder, ignore_errors=True)
+
+    return render_template("cpg.html", most_common_colors=most_common_colors)
 
 
 if __name__ == "__main__":
